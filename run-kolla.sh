2eed3fec1bdc11b75183119c0ea3546873c8a5c0
MrSchmaltz <schmaltz_sis@hotmail.com>
2023-11-01 13:24:27 +0000

    Update run-kolla.sh

--- run-kolla.sh
+++ run-kolla.sh
@@ -17,24 +17,24 @@ ssh-keygen -t rsa
 
 echo 'run-kolla.sh: Running ssh-copy-id openstack@controller1'
 ssh-copy-id openstack@controller1
-echo 'run-kolla.sh: Running ssh-copy-id openstack@controller2'
-ssh-copy-id openstack@controller2
+#echo 'run-kolla.sh: Running ssh-copy-id openstack@controller2'
+#ssh-copy-id openstack@controller2
 echo 'run-kolla.sh: Running ssh-copy-id openstack@compute001'
-ssh-copy-id openstack@compute001
+ssh-copy-id openstack@compute1
 
 echo 'run-kolla.sh: Running scp controller_setup.sh openstack@controller1:/home/openstack/controller_setup.sh'
 scp controller_setup.sh openstack@controller1:/home/openstack/controller_setup.sh
 echo 'run-kolla.sh: Running scp controller_setup.sh openstack@controller2:/home/openstack/controller_setup.sh'
 scp controller_setup.sh openstack@controller2:/home/openstack/controller_setup.sh
 echo 'run-kolla.sh: Running scp compute_setup.sh openstack@compute001:/home/openstack/compute_setup.sh'
-scp compute_setup.sh openstack@compute001:/home/openstack/compute_setup.sh
+scp compute_setup.sh openstack@compute1:/home/openstack/compute_setup.sh
 
 echo 'run-kolla.sh: Running ssh openstack@controller1 "sudo bash /home/openstack/controller_setup.sh"'
 ssh openstack@controller1 "sudo bash /home/openstack/controller_setup.sh"
 echo 'run-kolla.sh: Running ssh openstack@controller2 "sudo bash /home/openstack/controller_setup.sh"'
 ssh openstack@controller2 "sudo bash /home/openstack/controller_setup.sh"
 echo 'run-kolla.sh: Running ssh openstack@compute001 “sudo bash /home/openstack/compute_setup.sh”'
-ssh openstack@compute001 "sudo bash /home/openstack/compute_setup.sh"
+ssh openstack@compute1 "sudo bash /home/openstack/compute_setup.sh"
 
 # ---- PART TWO ----
 # Install Ansible and Kolla-Ansible
